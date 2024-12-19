20bbc5d56ef0b0b0d42850cf856526246324415d
Mrinal077 <mrinalpatil07@gmail.com>
2024-12-19 22:09:33 +0530

    changes for understanding

--- src/main/java/pack/intpalendrome.java
+++ src/main/java/pack/intpalendrome.java
@@ -25,3 +25,30 @@ public class intpalendrome {
 	}
 
 }
+package pack;
+
+public class intpalendrome {
+
+	public static void main(String[] args) {
+
+		String s= "12390321";
+		int i=0;
+		int j= s.length()-1;
+		
+		for (i=0; i<j;i++)
+		{
+			if(s.charAt(i)== s.charAt(j))
+			{
+				j--;
+			}
+			else {
+				System.out.println("it is not palendrome");
+				return;
+			}
+			
+		}
+		System.out.println("it is palendrome");
+		
+	}
+
+}
